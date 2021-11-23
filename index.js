8f0262c7563323a6408689a217ffb2642a6d79bc
odyslam.eth <odyslam@gmail.com>
2021-11-23 22:29:31 +0300

    Feat/send call (#1)

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ import { ethers } from 'ethers';
 let defaultHtml = `
 <!DOCTYPE html>
   <body>
-    <p> Various helpful APIs for sovereign individuals and Ethereum afficionados.</p>
+    <p> Various helpful APIs for sovereign individuals and Ethereum afficionados. By odyslam.eth.</p>
+    <p> All functionality uses your metamask provider, without ever having access to sensitive information. The client uses ethers-js.</p>
+    <p> You can easily inspect the source of this webpage to verify just how simple it is. It uses cloudflare workers to generate the HTML based on the URL.</p>
+    <p> <a href="https://github.com/odyslam/ethereum-worker-tools">View on GitHub and leave a star </a></p>
     <p><b>/sign/&lt;message&gt;:</b> Sign an arbitrary message with your web3 wallet (e.g metamask). It will return the signed message.</p>
     <p><b>/verify/&lt;address&gt;/&lt;signed_message&gt;/&lt;message&gt;:</b> Verifies that a signed message originates from the specific address.</p>
+    <p><b>/send/&lt;contract_address&gt;/&lt;function_signature&gt;/&lt;function_arguments&gt;:</b> Execute a smart contract's function by sending a transaction.<br>
+    <b>example:</b> /send/0x7EeF591A6CC0403b9652E98E88476fe1bF31dDeb/balanceOf(address,uint256) view returns(uint256)/"0x8DbD1b711DC621e1404633da156FcC779e1c6f3E" 42
+    </p>
+    <p><b>/call/&lt;contract_address&gt;/&lt;function_signature&gt;/&lt;function_arguments&gt;:</b> Call a smart contract's function without sending a transaction. It reads the state of the smart contract without changing the state on the blockchain.<br>
+    <b>example:</b> /send/0x7EeF591A6CC0403b9652E98E88476fe1bF31dDeb/balanceOf(address,uint256) view returns(uint256)/"0x8DbD1b711DC621e1404633da156FcC779e1c6f3E" 42
+    </p>
   <body>
 </html>
 `
@@ -38,6 +47,62 @@ let signHtml = `
   </body>
 </html>`
 
+let sendHtml = `
+<!DOCTYPE html>
+<script src="https://cdn.ethers.io/lib/ethers-5.2.umd.min.js"
+        type="application/javascript"></script>
+  <script>
+  async function sendTransaction(){
+      const enable = await window.ethereum.enable();
+      if(enable){
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        const contractAddress = "CONTRACT_ADDRESS";
+        const contractABI = ["function FUNCTION_SIGNATURE"];
+        const contract = new ethers.Contract(contractAddress, contractABI, signer);
+        let tx_info = await contract.FUNCTION_METHOD(METHOD_ARGUMENTS);
+        let tx_mined = await transaction.wait();
+        document.getElementById("tx_info").innerHTML = tx_info;
+        document.getElementById("tx_wait").innerHTML = tx_mined;
+        }
+      }
+      sendTransaction();
+    </script>
+<h1> Send a Transaction </h1>
+<p>
+  Transaction info: <span id="tx_info"></span> <br>
+  Transaction mined: <span id="tx_wait"></span> <br>
+  </p>
+  </body>
+</html>`
+
+let callHtml= `
+<!DOCTYPE html>
+<script src="https://cdn.ethers.io/lib/ethers-5.2.umd.min.js"
+        type="application/javascript"></script>
+  <script>
+  async function callTransaction(){
+      const enable = await window.ethereum.enable();
+      if(enable){
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const contractAddress = "CONTRACT_ADDRESS";
+        const contractABI = ["function FUNCTION_SIGNATURE"];
+        const contract = new ethers.Contract(contractAddress, contractABI, provider);
+        let tx_info = await contract.FUNCTION_METHOD(METHOD_ARGUMENTS);
+        document.getElementById("tx_info").innerHTML = tx_info;
+        }
+      }
+      callTransaction();
+    </script>
+<h1> Call a function</h1>
+<p>
+  Call result: <span id="tx_info"></span> <br>
+  </p>
+  </body>
+</html>
+
+
+`
 
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
@@ -63,7 +128,7 @@ async function handleRequest(request) {
     let verified = false;
     let  address = tokens[2];
     let signature = tokens[3];
-    let message = decodeURIComponent(tokens.slice(4).join("")).replaceAll(/"/g, '\\\"');
+    let message = decodeURIComponent(tokens.slice(4).join("")).replaceall(/"/g, '\\\"');
     let signedAddress = ethers.utils.verifyMessage(message, signature);
     if (signedAddress == address){
       verified = true;
@@ -89,6 +154,40 @@ async function handleRequest(request) {
       })
     }
   }
+  else if (pathname.startsWith("/send")) {
+    let tokens = pathname.split('/');
+    let address = tokens[2];
+    let signature = decodeURIComponent(tokens[3]);
+    let args = decodeURIComponent(tokens[4]);
+    args = args.replaceAll(" ", ",");
+    let method = signature.split('(')[0];
+    let input = sendHtml.replaceAll("CONTRACT_ADDRESS", address);
+    input = input.replaceAll("FUNCTION_SIGNATURE", signature);
+    input = input.replaceAll("FUNCTION_METHOD", method);
+    input = input.replaceAll("METHOD_ARGUMENTS", args);
+    return new Response(input, {
+        headers: {
+          "content-type": "text/html;charset=UTF-8",
+        },
+      })
+  }
+  else if (pathname.startsWith("/call")) {
+    let tokens = pathname.split('/');
+    let address = tokens[2];
+    let signature = decodeURIComponent(tokens[3]);
+    let args = decodeURIComponent(tokens[4]);
+    args = args.replaceAll(" ", ",");
+    let method = signature.split('(')[0];
+    let input = callHtml.replaceAll("CONTRACT_ADDRESS", address);
+    input = input.replaceAll("FUNCTION_SIGNATURE", signature);
+    input = input.replaceAll("FUNCTION_METHOD", method);
+    input = input.replaceAll("METHOD_ARGUMENTS", args);
+    return new Response(input, {
+        headers: {
+          "content-type": "text/html;charset=UTF-8",
+        },
+      })
+  }
   else {
     return new Response(defaultHtml, {
             headers: {
@@ -99,3 +198,8 @@ async function handleRequest(request) {
 
 
 }
+function isNumeric(value) {
+    return /^-?\d+$/.test(value);
+}
+
+
